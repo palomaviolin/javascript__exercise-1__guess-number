6329ad4d74e83d408e528daac74f964e6515d30b
Paloma Ania <paloma.aniabenito@gmail.com>
2018-12-03 18:07:41 +0100

    First code lines

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,42 @@
 'use strict';
 
-console.log('>> Ready :)');
+// Función que genera un número aleatorio hasta un máximo dado
+function getRandomNumber(max) {
+    return Math.ceil(Math.random() * max);
+  }
+  
+// La consola devuelve un número random hasta 100.
+console.log('> ' + getRandomNumber(100));
+
+// Para loguear el botón prueba (que aumente el número de veces que le doy al botón en la consola).
+
+const button = document.body.querySelector('#btn');
+
+function method(event) {
+    console.log(event);
+}
+
+button.addEventListener('click', method);
+
+
+//
+
+let number = '';
+
+if ( number > 50 ){
+  console.log('Demasiado alto'); 
+} else if ( number <= 50) {
+  console.log('Demasiado bajo'); 
+} else if ( number === /* NÚMERO RANDOM */) {
+  console.log('¡HAS GANADO, CAMPEONA!');
+} else {
+  console.log('Sigue intentándolo'); 
+
+
+// contador de intentos:
+let counter = document.getElementById("btn"),
+  count = 0;
+counter.onclick = function() {
+  count += 1;
+  counter.innerHTML = "Click me: " + count;
+};
